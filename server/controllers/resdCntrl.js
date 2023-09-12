b3b881ee9589540f13bae2877b5dc587e3371342
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-09-12 17:27:45 +0300

    residency endpoints

--- server/controllers/resdCntrl.js
+++ server/controllers/resdCntrl.js
@@ -21,4 +21,13 @@ export const createResidency = asyncHandler(async (req, res) => {
         throw new Error(error.message)
 
     }
-})
+});
+
+export const getAllResidencies = asyncHandler(async(req, res) => {
+    const residencies = await prisma.residency.findMany({
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+    res.send(residencies)
+});
